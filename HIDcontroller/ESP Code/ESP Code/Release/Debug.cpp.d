41259f29e8f21a507550a50cb41b31dbe678f592
David Cain <d.cain2740@gmail.com>
2020-05-23 01:53:18 -0500

    Fixing the name

--- HIDcontroller/ESP Code/ESP Code/Release/Debug.cpp.d
+++
@@ -1,3 +0,0 @@
-C:\Users\monke\AppData\Local\Temp\VMBuilds\ESP\ Code\esp32_nodemcu-32s\Release\Debug.cpp.o: \
- C:\Users\monke\AppData\Local\Temp\VMBuilds\ESP\ Code\esp32_nodemcu-32s\Release\Debug.cpp \
- C:\Users\monke\AppData\Local\Temp\VMBuilds\ESP\ Code\esp32_nodemcu-32s\Release\Debug.h
---
+++ HIDcontroller/HIDController/ESP Code/Release/Debug.cpp.d
@@ -0,0 +1,3 @@
+C:\Users\monke\AppData\Local\Temp\VMBuilds\HIDController\esp32_nodemcu-32s\Release\Debug.cpp.o: \
+ C:\Users\monke\AppData\Local\Temp\VMBuilds\HIDController\esp32_nodemcu-32s\Release\Debug.cpp \
+ C:\Users\monke\AppData\Local\Temp\VMBuilds\HIDController\esp32_nodemcu-32s\Release\Debug.h
